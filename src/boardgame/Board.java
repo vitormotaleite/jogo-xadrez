063b6e12deb96291d8b1f268d34dcc027b54ce99
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2025-05-03 08:50:05 -0300

    colocando peças no tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -35,4 +35,9 @@ public class Board {
     public Piece piece(Position position) {
         return pieces[position.getColumn()][position.getRow()];
     }
+
+    public void placePiece(Piece piece, Position position) {
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
 }
