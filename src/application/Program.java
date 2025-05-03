301c606cbe3253e33b839ab6c0ea32e13b11f34a
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2025-05-03 14:06:25 -0300

    tratamento de exeçoes e limpar tela

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,40 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class Program {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try{
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.peformChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.peformChessMove(source, target);
+            }
+            catch(ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch(InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
         
     }
--- src/application/UI.java
+++ src/application/UI.java
@@ -28,16 +28,21 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static ChessPosition readChessPosition(Scanner sc) {
         try {
             String s1 = sc.nextLine();
             char colmun = s1.charAt(0);
             int row = Integer.parseInt(s1.substring(1));
             return new ChessPosition(colmun, row);
-        } catch(RuntimeException e) {
+        } catch (RuntimeException e) {
             throw new InputMismatchException("Error reading chess position. valid values are from a1 to h8");
         }
-        
+
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
