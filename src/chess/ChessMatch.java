f7b3b3293fc6df83dbd6710541fa9d164cde1c29
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2025-05-04 15:46:37 -0300

    troca de jogador por turno

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,9 +9,13 @@ import chess.pieces.Rook;
 public class ChessMatch {
 
     private Board board;
+    private int turn;
+    private Color currentPlayer;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
     }
 
@@ -23,6 +27,14 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
     }
 
     public boolean[][] possibleMoves(ChessPosition sourcePosition) {
@@ -37,6 +49,7 @@ public class ChessMatch {
         validateSourcePosition(sourcePosition);
         validateTargetPosition(sourcePosition, targetPosition);
         Piece capturedPiece = makeMove(sourcePosition,targetPosition);
+        nextTurn();
         return (ChessPiece) capturedPiece;
     }
 
@@ -51,6 +64,9 @@ public class ChessMatch {
         if(!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
         }
+        if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+            throw new ChessException("The chosen piece is not yours");
+        }
         if(!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There is no possible moves for the chosen piece");
         }
@@ -60,6 +76,11 @@ public class ChessMatch {
         if(!board.piece(source).possibleMove(target)) {
             throw new ChessException("The chosen piece cannot move to target position");
         }
+    }
+
+    private void nextTurn() {
+        turn++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK: Color.WHITE;
     }
 
     private void placeNewPiece(char colmun, int row, ChessPiece piece) {
